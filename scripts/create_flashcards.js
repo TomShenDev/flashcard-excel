213f98855de546ab1bcb509e9c1dd6c65134aa8c
Taki Shen <tshin0346@gmail.com>
2023-05-29 09:26:59 -0400

    initial commit

---
+++ scripts/create_flashcards.js
@@ -0,0 +1,9 @@
+function CreateFlashCard(e){
+    e.preventDefault()
+    console.log("Creating flashcard!")
+    // read the first two row of the workbook
+    var reader = new FileReader();
+    let file = e.target.excelFile.files[0]
+    var workbook = XLSX.read(file, {type: 'binary', sheetRows: maxRow});
+    reader.readAsBinaryString(f);
+}
